b7e5ba5721b682922cfc16d705979e262eed0523
kojikawazu <nanairo7.daydream21@gmail.com>
2024-05-26 11:49:16 +0900

    render対応

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,4 +90,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # 環境変数からホスト名を取得して設定
+  config.hosts << ENV.fetch("ALLOWED_HOST", "rails-render-app.onrender.com")
 end
