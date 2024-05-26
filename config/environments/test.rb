f9fe8100ac47c42bf13fcb4974a8aef5a5eb377e
kojikawazu <nanairo7.daydream21@gmail.com>
2024-05-26 12:32:33 +0900

    render対応

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -57,4 +57,7 @@ Rails.application.configure do
 
   # Annotate rendered view with file names.
   # config.action_view.annotate_rendered_view_with_filenames = true
+
+  # allowed host list
+  config.hosts << "rails-render-app.onrender.com"
 end
